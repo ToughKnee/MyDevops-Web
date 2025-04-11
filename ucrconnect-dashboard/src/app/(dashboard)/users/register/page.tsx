ab8aeed85e73188e01e2946ce215acea78528897
BryanVillegas <bryan.villegasalvarado@ucr.ac.cr>
2025-04-10 18:02:18 -0600

    feat(user): Enhance registration form with confirm password field and validation

--- ucrconnect-dashboard/src/app/(dashboard)/users/register/page.tsx
+++ ucrconnect-dashboard/src/app/(dashboard)/users/register/page.tsx
@@ -7,6 +7,7 @@ export default function RegisterUser() {
     name: '',
     email: '',
     password: '',
+    confirmPassword: '',
   });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -15,7 +16,12 @@ export default function RegisterUser() {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    
+
+    if (formData.password !== formData.confirmPassword) {
+      alert("Las contraseñas no coinciden");
+      return;
+    }
+
     // Fetch API to register user
     console.log(formData);
   };
@@ -23,51 +29,68 @@ export default function RegisterUser() {
   return (
     <div>
       <h2 className="mt-2 text-gray-600">Registrar nuevo usuario</h2>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4 text-gray-800">
-        <div>
+      <div className="mt-10 w-full sm:w-11/12 md:w-3/4 lg:w-1/3">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4 text-gray-800">
+          <div>
             <label htmlFor="name" className="block text-sm font-medium text-gray-700"> Nombre </label>
             <input
-            id="name"
-            name="name"
-            type="text"
-            placeholder="Nombre"
-            value={formData.name}
-            onChange={handleChange}
-            required
-            className="mt-1 block px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+              id="name"
+              name="name"
+              type="text"
+              placeholder="Nombre"
+              value={formData.name}
+              onChange={handleChange}
+              required
+              className="mt-1 w-full block px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
             />
-        </div>
-        <div>
+          </div>
+          <div>
             <label htmlFor="email" className="block text-sm font-medium text-gray-700"> Correo electrónico </label>
             <input
-            id="email"
-            name="email"
-            type="email"
-            placeholder="Correo electrónico"
-            value={formData.email}
-            onChange={handleChange}
-            required
-            className="mt-1 block px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Correo electrónico"
+              value={formData.email}
+              onChange={handleChange}
+              required
+              className="mt-1 w-full block px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
             />
-        </div>
-        <div>
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700"> Contraseña </label>  
+          </div>
+          <div>
+            <label htmlFor="password" className="block text-sm font-medium text-gray-700"> Contraseña </label>
             <input
-            id="password"
-            name="password"
-            type="password"
-            placeholder="Contraseña"
-            value={formData.password}
-            onChange={handleChange}
-            required
-            className="mt-1 block px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+              id="password"
+              name="password"
+              type="password"
+              placeholder="Contraseña"
+              value={formData.password}
+              onChange={handleChange}
+              required
+              className="mt-1 w-full block px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
             />
-        </div>
+          </div>
+          <div>
+            <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">Confirmar Contraseña</label>
+            <input
+              id="confirmPassword"
+              type="password"
+              name="confirmPassword"
+              placeholder="Confirmar contraseña"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+              required
+              className="mt-1 w-full block px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+            />
+          </div>
 
-        <button type="submit" className="w-auto py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-          Registrarse
-        </button>
-      </form>
+          <div className="flex justify-center">
+            <button type="submit" className="mt-2 w-auto py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+              Registrar usuario
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
