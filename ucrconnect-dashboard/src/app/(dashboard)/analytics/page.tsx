7b2449b53e3427b64ef1e9bf2695cd71044b27cd
Julian2603 <julian.robertsolano@ucr.ac.cr>
2025-04-15 13:12:20 -0600

    feat(auth): add recover password page

--- ucrconnect-dashboard/src/app/(dashboard)/analytics/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Analytics() {
-  return (
-    <div>
-      <h1 className="text-gray-800">Analytics</h1>
-    </div>
-  );
-}
--- ucrconnect-dashboard/src/app/(dashboard)/content/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Content() {
-  return (
-    <div>
-      <h1 className="text-gray-800">Content</h1>
-    </div>
-  );
-}
