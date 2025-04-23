0b7eaeafb22bd42e5fcfb7a8fe4c16da6462ad62
AllisonFosterMcLeod <allison.foster@ucr.ac.cr>
2025-04-23 15:04:20 -0600

    Revert "Update test coverage validation"
    This reverts commit 3161477b71dcb1c06efef208470e197937b9445b.

--- ucrconnect-dashboard/jest.config.js
+++ ucrconnect-dashboard/jest.config.js
@@ -29,10 +29,10 @@ const customJestConfig = {
   ],
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      branches: 40,
+      functions: 40,
+      lines: 40,
+      statements: 40,
     },
   },
   // Next.js 13+ specific files
